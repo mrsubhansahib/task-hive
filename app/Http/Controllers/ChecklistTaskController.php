f02dd3d2f9cac2141e44a266956257e1594dbb96
muhammadhamad123 <muhammadhamadjb99@gmail.com>
2024-12-24 17:28:01 +0500

    cards,checklist,checklist-tasks

---
+++ app/Http/Controllers/ChecklistTaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChecklistTask;
+use Illuminate\Http\Request;
+
+class ChecklistTaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ChecklistTask $checklist_task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ChecklistTask $checklist_task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ChecklistTask $checklist_task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ChecklistTask $checklist_task)
+    {
+        //
+    }
+}

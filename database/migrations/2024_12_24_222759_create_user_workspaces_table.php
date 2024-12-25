4839c97bac218434fcfdbd990c8c441cadb3beaf
Namal Laptop <namalniamat5648@gmail.com>
2024-12-24 17:39:14 -0800

    change id

--- database/migrations/2024_12_24_222759_create_user_workspaces_table.php
+++ database/migrations/2024_12_24_222759_create_user_workspaces_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_workspaces', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->string('workspace_id');
+            $table->foreignId('workspace_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
